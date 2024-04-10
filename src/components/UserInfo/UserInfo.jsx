37835b4ebe519c45595100ede1f8d3633d9b8834
ALion Alex <alionalexxx@gamil.com>
2024-04-11 00:15:19 +0300

    fix solution

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './UserInfo.scss';
 
-export const UserInfo = ({ user }) => {
-  const mailLink = `mailto:${user.email}`;
-
-  return (
-    <a className="UserInfo" href={mailLink}>
-      {user.name}
-    </a>
-  );
-};
+export const UserInfo = ({ name, email }) => (
+  <a
+    className="UserInfo"
+    href={`mailto:${email}`}
+  >
+    {name}
+  </a>
+);
